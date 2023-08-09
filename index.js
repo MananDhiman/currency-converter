ec0d9f6ba44160611ee2de9e33d7cc9d69852ae0
Manan Dhiman <manandheman@gmail.com>
2023-08-09 18:28:47 +0530

    Added currency conversion

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ main();
 
 function main() {
 
+    // show a loading screen here
+
     getParsedDataCollection().then( list => {
 
         const allCurrencies = list[0];
@@ -18,7 +20,17 @@ function main() {
         createMapFromCurrencyData(map, allCurrencies, eurRate);
 
         createHtmlTableFromMap(map, allCurrencies);
+        
+        setCurrenciesInSelect(map);
+
+        const convertButton = document.getElementById("convert_currency_1_currency_2");
+        convertButton.onclick = function() {
+            convertCurrency(map);
+        };
+
     });
+
+    // end the loading screen here
 
 }
 
@@ -43,13 +55,7 @@ async function getJsonFromApi(url) {
 function createMapFromCurrencyData(map, all, eurRate) {
 
     Object.keys(all).forEach(function(key){
-        map.set(key, null);
-    });
-
-    Object.keys(eurRate).forEach(function(key){
-        if(map.has(key)) {
-            map.set(key, eurRate[key]);
-        }
+        map.set(key, eurRate[key]);
     });
 
 }
@@ -63,10 +69,55 @@ function createHtmlTableFromMap(map, allCurrencies) {
     let tableBody = "";
 
     Object.keys(allCurrencies).forEach(function(key){
-        tableBody += `<tr><td>${key}</td><td>${allCurrencies[key]}</td><td>${map.get(key)}</td></tr>` 
+        tableBody += `<tr><td>${key}</td><td>${allCurrencies[key]}</td><td>${map.get(key)}</td></tr>`;
     });
 
     const completeTable = tableHeader + tableBody;
 
     table.innerHTML = completeTable;
+}
+
+function setCurrenciesInSelect(map) {
+    const select_1 = document.getElementById("currency_1");
+    const select_2 = document.getElementById("currency_2");
+
+    let data = "<option value='--'>--</option>";    
+
+    map.forEach(function (value, key) {
+        data += `<option value="${key}">${key}</option>`;
+    });
+
+    select_1.innerHTML = data;
+    select_2.innerHTML = data;
+
+}
+
+function convertCurrency(map) {
+    const select_1 = document.getElementById("currency_1");
+    const select_2 = document.getElementById("currency_2");
+    const amount_1 = document.getElementById("amount_1");
+
+    const value_1 = select_1.value;
+    const value_2 = select_2.value;
+
+    let amount_2 = document.getElementById("amount_2");
+
+    if (areOptionsSelected(value_1, value_2)) {
+        
+        let ratio = map.get(value_2) / map.get(value_1);
+        let convertedAmount = (ratio * amount_1.value).toFixed(2);
+        amount_2.textContent = convertedAmount;
+
+    } else {
+        alert("Please select both currencies")
+    }
+
+}
+
+function areOptionsSelected(value_1, value_2) {
+
+    if (value_1 === "--" || value_2 === "--") return false;
+
+    return true;
+
 }
